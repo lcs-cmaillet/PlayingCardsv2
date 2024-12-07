7b18a4d673b7b4fba36451904a6a9516c5112190
Collin Maillet <cmaillet@lcs.on.ca>
2024-12-06 19:14:22 -0500

    Initial Commit

---
+++ PlayingCards v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayingCards v2
+//
+//  Created by Collin Maillet on 2024-12-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayingCards v2/PlayingCards_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingCards_v2App.swift
+//  PlayingCards v2
+//
+//  Created by Collin Maillet on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct PlayingCards_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
