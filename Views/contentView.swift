edd0fb651123f8469ec0accb76cd033bed28d9c7
Collin Maillet <cmaillet@lcs.on.ca>
2024-12-10 13:39:42 -0500

    added a bottom rounded reqtangle to my trading card.

--- Views/contentView.swift
+++ Views/contentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            UnevenRoundedRectangle(cornerRadii: .init(bottomLeading: 50, bottomTrailing: 50))
+                            .fill(.orange)
+            Text("James Turner")
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
---
+++ shapes/block.swift
@@ -0,0 +1,15 @@
+//
+//  block.swift
+//  PlayingCards v2
+//
+//  Created by Collin Maillet on 2024-12-06.
+//
+import SwiftUI
+
+struct block: Shape {
+    func path(in rect: CGRect) -> Path{
+        var path = Path()
+        
+        return path
+    }
+}
